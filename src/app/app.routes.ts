cca3d47401ef9d8b2f4183a33b2fb21474ef18a3
Your Name <yourgithubemail@gmail.com>
2026-02-21 20:35:35 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Navbar } from './shared/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+];
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -4,22 +4,10 @@ import { CommonModule } from "../../../../node_modules/@angular/common";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  isMenuOpen = false;
-  isServiceOpen= false;
-  isProductOpen=false;
-  toggleMenu(){
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-  toggleServices(){
-    this.isServiceOpen = !this.isServiceOpen;
-    
-  }
-  toggleProducts(){
-    this.isProductOpen = !this.isProductOpen;
-  }
+  
 }
